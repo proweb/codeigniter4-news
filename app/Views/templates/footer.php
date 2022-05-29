47300495175689b09e9693af74fd0d4f318417cb
Sergey Mochalov <bizezine@gmail.com>
2022-05-29 17:18:49 +0300

    Update for Views

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,6 +1,8 @@
 
-                
+
     </div></section>
 <em>&copy; <?= date("Y"); ?></em>
+    <script src="<?= base_url('js/uikit.min.js') ?>"></script>
+    <script src="<?= base_url('js/uikit-icons.min.js') ?>"></script>
 </body>
 </html>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,18 +1,16 @@
 <!doctype html>
 <html>
 <head>
-    <title>CodeIgniter Tutorial</title>
+    <title><?= esc($title) ?></title>
     <link rel="stylesheet" href="<?= base_url('css/uikit.min.css') ?>">
-    <script src="<?= base_url('js/uikit.min.js') ?>"></script>
-<script src="<?= base_url('js/uikit-icons.min.js') ?>"></script>
 </head>
 <body>
-    <header class="uk-background-primary uk-light">
-        <div class="uk-container uk-padding">
+<header class="uk-background-primary uk-light">
+    <div class="uk-container uk-padding">
         <h1><?= esc($title) ?></h1>
-        </div>
-    </header>
-    <section class="uk-section">
-        <div class="uk-container">
-    
+    </div>
+</header>
+<section class="uk-section">
+    <div class="uk-container">
+
 
