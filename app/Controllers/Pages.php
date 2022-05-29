3dfa425f36e7a63bc0335067e56d9e5c48c6a981
Sergey Mochalov <bizezine@gmail.com>
2022-05-29 17:19:56 +0300

    Refactor controller

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -6,16 +6,16 @@ class Pages extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $this->view();
     }
 
     public function view($page = 'home')
     {
-        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+        if (!is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
             // Whoops, we don't have a page for that!
             throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
         }
-        
+
         $data['title'] = ucfirst($page); // Capitalize the first letter
         echo view('templates/header', $data);
         echo view('pages/' . $page, $data);
