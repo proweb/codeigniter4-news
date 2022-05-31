bf794273672c065a168bb40c04dbd9fda825b4ef
Sergey Mochalov <bizezine@gmail.com>
2022-05-31 21:37:53 +0300

    Add new Routes for create method

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,6 +29,7 @@ $routes->get('/', 'Pages::index');
 $routes->get('/home', function () {
 	return redirect()->to(base_url());
 });
+$routes->match(['get', 'post'], 'news/create', 'News::create');
 $routes->get('news/(:segment)', 'News::view/$1');
 $routes->get('news', 'News::index');
 $routes->get('(:any)', 'Pages::view/$1');
