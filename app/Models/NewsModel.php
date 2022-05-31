181dd0e797df10bb9cec76afe234e6ecfa3d18d2
Sergey Mochalov <bizezine@gmail.com>
2022-05-31 21:36:34 +0300

    $allowedFields in model

--- app/Models/NewsModel.php
+++ app/Models/NewsModel.php
@@ -6,7 +6,8 @@ use CodeIgniter\Model;
 
 class NewsModel extends Model {
 	protected $table = 'news';
-
+	protected $allowedFields = ['title', 'slug', 'body'];
+	// Get item from DB
 	public function getNews($slug = FALSE)
 	{
 		if ($slug === FALSE){
