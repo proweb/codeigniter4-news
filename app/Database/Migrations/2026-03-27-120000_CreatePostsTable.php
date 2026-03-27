5394504d99620cabe635163a0f0c2ae76bf1241e
Sergey Mochalov <greyus123@sourcecraft.dev>
2026-03-28 00:32:40 +0300

    first migration file for posts table

---
+++ app/Database/Migrations/2026-03-27-120000_CreatePostsTable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePostsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'slug' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'body' => [
+                'type' => 'TEXT',
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addUniqueKey('slug');
+        $this->forge->createTable('posts');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('posts');
+    }
+}
